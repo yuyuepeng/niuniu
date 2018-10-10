7e378f957d1a6327d42d343bf6b1b0398f2d86d0
yuyuepeng <yuyuepeng@hexindai.com>
2018-10-10 13:56:51 +0800

    no message

---
+++ swiftProject/NewTabbarController.swift
@@ -0,0 +1,33 @@
+//
+//  NewTabbarController.swift
+//  swiftProject
+//
+//  Created by 玉岳鹏 on 2018/10/10.
+//  Copyright © 2018年 玉岳鹏. All rights reserved.
+//
+
+import UIKit
+
+class NewTabbarController: UITabBarController,UITabBarControllerDelegate {
+    let singleLength:CGFloat = YYScreenWidth/750.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func setUpViewControllers() -> Void {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftProject
+//
+//  Created by 玉岳鹏 on 2018/10/9.
+//  Copyright © 2018年 玉岳鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
