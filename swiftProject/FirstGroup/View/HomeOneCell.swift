a9582a2ebf8d424741883cde2168b9bf87917a78
yuyuepeng <yuyuepeng@hexindai.com>
2018-10-10 18:03:28 +0800

    no message

---
+++ swiftProject/FirstGroup/View/HomeOneCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeOneCell.swift
+//  swiftProject
+//
+//  Created by 玉岳鹏 on 2018/10/10.
+//  Copyright © 2018年 玉岳鹏. All rights reserved.
+//
+
+import UIKit
+
+class HomeOneCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
